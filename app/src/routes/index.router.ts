1b898d08aff287932fa853cd1ab925874d349fe1
cabama <barreymata@gmail.com>
2018-08-30 22:54:18 +0200

    format document

--- app/src/routes/index.router.ts
+++ app/src/routes/index.router.ts
@@ -5,54 +5,52 @@ import { LoginRouter } from './login.router';
 import { UserRouter } from './user.router';
 
 export class Routing {
-	
-	private router = Router();
-	private loginRouter: LoginRouter;
-	private userRouter: UserRouter;
 
-	constructor () {
-		this.router.use(this.enableCors)
-		this.loginRouter = new LoginRouter()
-		this.userRouter = new UserRouter()
-		this.router.use('/login', this.loginRouter.routing)
-		this.router.use('/users', this.userRouter.routing)
-		this.router.use('/', this.setRoutes)
-	}
+  private router = Router();
+  private loginRouter: LoginRouter;
+  private userRouter: UserRouter;
 
-	enableCors(req, res, next) {
-			// Website you wish to allow to connect
-			res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-			res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-			res.setHeader('Access-Control-Allow-Credentials', 'true');
-			// Pass to next layer of middleware
-			next();
-	}
+  constructor()  {
+    this.router.use(this.enableCors)
+    this.loginRouter = new LoginRouter()
+    this.userRouter = new UserRouter()
+    this.router.use('/login', this.loginRouter.routing)
+    this.router.use('/users', this.userRouter.routing)
+    this.router.use('/', this.setRoutes)
+  }
 
-	get setRoutes () {
-		this.router.get('/',   // This request must be authenticated using a JWT, or else we will fail
-			LoginRouter.isAuth(),
-			(req, res) => {
-				console.log(req.user)
-				res.json(req.user.id)
-			}, (req, res) => res.json(req.user))
-		this.router.get('/reshu', (req, res) => res.json({ hola: 'pues esto es asi cazador sin ' }))
-		this.router.get('/hormiga', (req, res) => res.json({ hola: 'hola soy una hormiga' }))
-		this.router.get('/atleti', (req, res) => res.json({ chucufleta: 'Que sepas que el Atleti es el mejor equipo del mundo' }))
+  enableCors(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // Pass to next layer of middleware
+    next();
+  }
 
-		// this.router.get('/login', (req, res) => res.json({ login: 'No estas logueado guapeton' }));
-		this.router.get('/google/callback',
-			passport.authenticate('google', { failureRedirect: '/api/login' }),
-			function (req, res) {
-				console.log('Google ', req.user)
-				res.json({ token: generateAccessToken(req.user.userid) });
-			});
-			
-		return this.router
-	}
-	
-	get enrouting () { return this.router }
-	
+  get setRoutes() {
+    this.router.get('/',   // This request must be authenticated using a JWT, or else we will fail
+      LoginRouter.isAuth(),
+      (req, res) => {
+        console.log(req.user)
+        res.json(req.user.id)
+      }, (req, res) => res.json(req.user))
+    this.router.get('/hormiga', (req, res) => res.json({ hola: 'hola soy una hormiga' }))
+
+    // this.router.get('/login', (req, res) => res.json({ login: 'No estas logueado guapeton' }));
+    this.router.get('/google/callback',
+      passport.authenticate('google', { failureRedirect: '/api/login' }),
+      function (req, res) {
+        console.log('Google ', req.user)
+        res.json({ token: generateAccessToken(req.user.userid) });
+      });
+
+    return this.router
+  }
+
+  get enrouting() { return this.router }
+
 }
 
 
