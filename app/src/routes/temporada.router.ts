4e7a1151e82ea092d513dfb7996585f182bdbae1
Carlos Barreiro Mata <barreymata@gmail.com>
2018-11-21 04:36:56 +0100

    ci test

--- app/src/routes/temporada.router.ts
+++ app/src/routes/temporada.router.ts
@@ -8,7 +8,7 @@ export class TemporadaRouter {
 
   constructor () {
     this.router.get('/calendar', (req, res) => this.Calendar(res));
-    this.router.get('/example', (req, res) => res.json({hola: 'caraacola'}));
+    this.router.get('/example', (req, res) => res.json({hola: 'caracola'}));
   }
 
   get routing () { return this.router } 
